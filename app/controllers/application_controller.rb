98cc2388b49ac0279871a8a884488994c8557396
Dominik Stodolny <d.stodolny@gmail.com>
2015-04-25 14:14:02 +0200

    remove profile_id and profile_type from devise strong parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up).push(:profile_id, :profile_type, :firstname, :lastname)
-    devise_parameter_sanitizer.for(:account_update).push(:profile_id, :profile_type, :firstname, :lastname)
+    devise_parameter_sanitizer.for(:sign_up).push(:firstname, :lastname)
+    devise_parameter_sanitizer.for(:account_update).push(:firstname, :lastname)
   end
 end
