c9547f2cbbea75a0cff2d1b6a3830f59f342fca4
Dominik Stodolny <d.stodolny@gmail.com>
2015-04-25 12:20:52 +0200

    fix permitted parameters for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up).push(:profile_id, :profile_type, :firstname, :lastname)
-    devise_parameter_sanitizer.for(:sign_up).push(:profile_id, :profile_type, :firstname, :lastname)
+    devise_parameter_sanitizer.for(:account_update).push(:profile_id, :profile_type, :firstname, :lastname)
   end
 end
