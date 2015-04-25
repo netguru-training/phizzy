e07031bb9c00d36126b126f170c9861cb7ead93f
Dominik Stodolny <d.stodolny@gmail.com>
2015-04-25 14:59:11 +0200

    connect exercises to patient profile

--- app/models/exercise_patient.rb
+++ app/models/exercise_patient.rb
@@ -1,6 +1,6 @@
 class ExercisePatient < ActiveRecord::Base
 	belongs_to :exercise
-	# belongs_to: :patients_profile
+	belongs_to :patients_profile
 	validates_presence_of :exercise_id
 	validates_presence_of :patient_id
 	validates_presence_of :series_count 
--- app/models/patient_profile.rb
+++ app/models/patient_profile.rb
@@ -1,5 +1,7 @@
 class PatientProfile < ActiveRecord::Base
   has_one :user, as: :profilable
+  has_many :exercise_patients, foreign_key: :patient_id
+  has_many :exercises, through: :exercise_patients
 
   validates_presence_of :user
 end
