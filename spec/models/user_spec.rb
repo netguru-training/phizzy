0c99625e3a03d2d490b70265f54703f93cd31b8d
Dominik Stodolny <d.stodolny@gmail.com>
2015-04-25 14:39:08 +0200

    test polymorphic associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { FactoryGirl.create(:user) }
+
   it "has a valid factory" do
     expect(FactoryGirl.build(:user)).to be_valid
+  end
+
+  context "as a therapist" do
+    let(:therapist_profile) { FactoryGirl.create(:therapist_profile) }
+
+    before do
+      user.profilable = therapist_profile
+      user.save
+    end
+
+    it "has a valid profile" do
+      expect(user.profilable).to eq therapist_profile
+    end
+  end
+
+  context "as a patient" do
+    let(:patient_profile) { FactoryGirl.create(:patient_profile) }
+
+    before do
+      user.profilable = patient_profile 
+      user.save
+    end
+
+    it "has a valid profile" do
+      expect(user.profilable).to eq patient_profile
+    end
   end
 
   describe "validations" do
