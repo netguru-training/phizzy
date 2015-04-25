72006227547bec69e626ab62fea1da99573472f3
Pawel <pavel.jarema@gmail.com>
2015-04-25 12:48:04 +0200

    add routes and controller actions to profiles

--- app/controllers/patient_profile_controller.rb
+++ app/controllers/patient_profile_controller.rb
@@ -1,2 +1,40 @@
 class PatientProfileController < ApplicationController
+	before_filter :check_user
+
+	def show
+		@profile = current_user.patient_profile
+	end
+
+	def create
+		if PatientProfile.create(profile_params)
+			redirect_to @profile
+		end
+	end
+
+	def edit
+		@profile = PatientProfile.find(params[:id])
+	end
+
+	def update
+		@profile = PatientProfile.find(params[:id])
+		if @profile.update_params(profile_params)
+			flash[:notice] = 'Profile updated'
+			redirect_to @profile
+		else
+			render 'edit'
+		end
+	end
+
+	private	
+
+		def profile_params
+			params.require(:patient_profile).permit(:theraist_id)
+		end
+
+		def check_user
+			unless user_signer_in?
+				flash[:notice] = 'Please log in to view the profile'
+				redirect_to new_user_session_path
+			end
+		end
 end
--- app/controllers/therapist_profile_controller.rb
+++ app/controllers/therapist_profile_controller.rb
@@ -1,2 +1,40 @@
 class TherapistProfileController < ApplicationController
+	before_filter :check_user
+
+	def show
+		@profile = current_user.theraphist_profile
+	end
+
+	def create
+		if TheraphistProfile.create(profile_params)
+			redirect_to theraphist_profile_show(@profile)
+		end
+	end
+
+	def edit
+		@profile = TheraphistProfile.find(params[:id])
+	end
+
+	def update
+		@profile = TheraphistProfile.find(params[:id])
+		if @profile.update_params(profile_params)
+			flash[:notice] = 'Profile updated'
+			redirect_to @profile
+		else
+			render 'edit'
+		end
+	end
+
+	private	
+
+		def profile_params
+			params.require(:therapist_profile).permit(:address, :hours_from, :hours_to)
+		end
+
+		def check_user
+			unless user_signer_in?
+				flash[:notice] = 'Please log in to view the profile'
+				redirect_to new_user_session_path
+			end
+		end
 end
