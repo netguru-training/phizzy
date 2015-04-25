7274e96bf7924bf0ac46989f16130cd069dce005
gtalkowski <g.talkowski@gmail.com>
2015-04-25 16:19:35 +0200

    Remove unused links

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'exercise/new'
-
-  get 'exercise/edit'
-
-  get 'exercise/delete'
-
   devise_for :users, controllers: { registrations: 'registrations' }
   root to: 'visitors#index'
   resource :patient_profile, only: [:show, :edit, :update, :create]
