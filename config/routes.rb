b1c018b07a0e1d0f754c899c3a1cb709e3d00b93
Pawel <pavel.jarema@gmail.com>
2015-04-25 14:44:24 +0200

    correct routes for custom devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   root to: 'visitors#index'
 
   resources :therapist_profiles, :patient_profiles, only: [:show, :edit, :update, :create]
