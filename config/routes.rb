738f222be33fe8f3aaade39b0411b7fcc7919fb8
gtalkowski <g.talkowski@gmail.com>
2015-04-26 10:03:10 +0200

    Add exercises controller and remove unused files

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'registrations' }
   root to: 'visitors#index'
+
   resources :patient_profiles, only: [:index, :show, :edit, :update, :create]
   resources :exercise_patient, only: [:new, :edit, :delete]
-  resources :exercise, only: [:new, :edit, :delete]
   resource :therapist_profile, only: [:show, :edit, :update, :create]
+  resource :exercises
 end
