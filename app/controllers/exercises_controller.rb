af3b2c51ae6996122e9b5c712f14ee5728647baf
gtalkowski <g.talkowski@gmail.com>
2015-04-26 10:33:18 +0200

    update views and exercises controller

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,4 +1,42 @@
 class ExercisesController < ApplicationController
-  def new
-  end
+  	
+  	expose(:exercise)
+    expose(:exercises)
+    
+    def index
+    end
+
+    def new      
+  	end
+
+  	def edit
+  	end
+
+  	def create
+  	  self.exercise = Exercise.new(exercise_params)
+
+      if exercise.save
+        redirect_to exercises, notice: 'Exercise was successfully created.'
+      else
+        render action: 'new'
+      end
+    end
+
+    def update
+      if exercise.update(exercise_params)
+        redirect_to exercise, notice: 'Exercise was successfully updated.'
+      else
+        render action: 'edit'
+      end
+    end
+
+    def destroy
+      exercise.destroy
+      redirect_to exercises_url, notice: 'Exercise was successfully destroyed.'
+    end
+
+	private
+      def exercise_params
+        params.require(:exercise).permit(:name, :description)
+      end
 end
